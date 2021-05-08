cc86510fc21763552d4894c5f6c573216557b583
Adam Erispaha <aerispaha@gmail.com>
2021-05-08 16:31:45 -0400

    add nouns n verbs and basic sentence

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
+import csv
+import os
+import random
+
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
 
+with open(os.path.join('data', 'nouns.csv'), newline='') as f:
+    reader = csv.reader(f)
+    nouns = [n[0] for n in list(reader)]
+
+with open(os.path.join('data', 'verbs.csv'), newline='') as f:
+    reader = csv.reader(f)
+    verbs = [v[0] for v in list(reader)]
+
 
 @app.route("/")
 def hello_world():
+
+    subject_1 = random.choice(nouns)
+    subject_2 = random.choice(nouns)
+    verb_1 = random.choice(verbs)
+    verb_2 = random.choice(verbs)
+
+    s = f'The {subject_1} {verb_1}s and the {subject_2} {verb_2}s.'
+    return s
     return render_template("index.html")
