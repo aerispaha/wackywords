6bc483ff14ea57e557d927347eadda2682c0252c
Adam Erispaha <aerispaha@gmail.com>
2021-05-08 17:07:05 -0400

    improve ui and add sentene structures

--- app.py
+++ app.py
@@ -23,7 +23,15 @@ def hello_world():
     subject_2 = random.choice(nouns)
     verb_1 = random.choice(verbs)
     verb_2 = random.choice(verbs)
+    pronoun_1 = random.choice(['they', 'I', 'you', 'we'])
+    pronoun_2 = random.choice(['he', 'she'])
 
-    s = f'The {subject_1} {verb_1}s and the {subject_2} {verb_2}s.'
-    return s
-    return render_template("index.html")
+    sentences = [
+        f'The {subject_1} {verb_1}s and the {subject_2} {verb_2}s.',
+        f'Why does the {subject_1} {verb_1} while the {subject_2} {verb_2}s?',
+        f'Can a {subject_1} {verb_1}?',
+        f'Is {verb_1}ing possible when {pronoun_1} {verb_2}?',
+        f'Is {verb_1}ing possible when {pronoun_2} {verb_2}s?'
+    ]
+
+    return render_template("index.html", sentence=random.choice(sentences))
