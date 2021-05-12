912328190585cbfb42cdefe953c60df707a1f260
Adam Erispaha <aerispaha@gmail.com>
2021-05-11 23:00:17 -0400

    add adverbs, some grammar, preposition and conjunction randomization

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ def word_list_from_file(filepath) -> list:
 nouns = word_list_from_file(os.path.join('data', 'nouns.csv'))
 verbs = word_list_from_file(os.path.join('data', 'verbs.csv'))
 adjectives = word_list_from_file(os.path.join('data', 'adjectives.csv'))
+adverbs = word_list_from_file(os.path.join('data', 'adverbs.csv'))
 
 
 @app.route("/")
@@ -30,15 +31,28 @@ def hello_world():
     verb_2 = random.choice(verbs)
     pronoun_1 = random.choice(['they', 'I', 'you', 'we'])
     pronoun_2 = random.choice(['he', 'she'])
+    conjunction = random.choice(['when', 'while', 'but', 'and', 'or'])
+    preposition = random.choice(['a', 'the', 'our', 'their'])
+    adverb = random.choice(adverbs)
+    adjective = random.choice(adjectives)
 
     sentences = [
-        f'The {subject_1} {verb_1}s and the {subject_2} {verb_2}s.',
-        f'Why does the {subject_1} {verb_1} while the {subject_2} {verb_2}s?',
-        f'Can a {subject_1} {verb_1}?',
-        f'Is {verb_1}ing {random.choice(adjectives)} when {pronoun_1} {verb_2}?',
-        f'Is {verb_1}ing {random.choice(adjectives)} when {pronoun_2} {verb_2}s?',
-        f'A {random.choice(adjectives)} {subject_1} is {verb_1}ing when the {subject_2} {verb_2}s.',
-        f'Is it {random.choice(adjectives)} to {verb_1}?',
+        f'{preposition.capitalize()} {subject_1} {verb_1}s {conjunction} the {subject_2} {verb_2}s.',
+        f'Why does {preposition} {subject_1} {verb_1} {conjunction} the {subject_2} {verb_2}s?',
+        f'Can {preposition} {subject_1} {verb_1}?',
+        f'Is {verb_1}ing {adjective} when {pronoun_1} {verb_2}?',
+        f'Is {verb_1}ing {adjective} when {pronoun_2} {verb_2}s?',
+        f'{preposition.capitalize()} {adjective} {subject_1} is {verb_1}ing {conjunction} the {subject_2} {verb_2}s.',
+        f'Is it {adjective} to {verb_1}?',
+        f'{pronoun_1.capitalize()} {verb_1} {adverb}.'
     ]
 
-    return render_template("index.html", sentence=random.choice(sentences))
+    # grammar hacks
+    words = random.choice(sentences).split()
+    words = [w.replace('sss', 'sses') if w.endswith('sss') else w for w in words]
+    words = [w.replace('chs', 'ches') if w.endswith('chs') else w for w in words]
+    words = [w.replace('eing', 'ing') if w.endswith('eing') else w for w in words]
+
+    sentence = ' '.join(words)
+
+    return render_template("index.html", sentence=sentence)
